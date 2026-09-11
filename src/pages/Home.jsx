031f624a4b2d922f82997f560c9fdf1ea8ed928d
Amruth <amruth@blueseed.co.in>
2026-09-12 01:53:54 +0530

    Remove duplicate logo from badge, keep only in card body

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -62,9 +62,7 @@ export default function Home() {
         </div>
         <div className="company-card reveal rd1">
           <div className="cc-left">
-            <div className="cc-badge">
-              <img src="/zeroempty-logo.svg" alt="ZeroEmpty" className="cc-badge-logo" />
-            </div>
+            <div className="cc-badge">ZE</div>
             <div className="cc-meta">
               <span className="cc-num">01</span>
               <span className="cc-sector">Logistics & Freight</span>
